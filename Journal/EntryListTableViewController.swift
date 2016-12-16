d70049f8ee891307e944dc60a393f9a660d8147c
Andrew Madsen <andrew@openreelsoftware.com>
2016-12-16 10:46:59 -0700

    Fix unnecessary cast to NSIndexPath.

--- Journal/EntryListTableViewController.swift
+++ Journal/EntryListTableViewController.swift
@@ -25,7 +25,7 @@ class EntryListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "entryCell", for: indexPath)
 
-        let entry = EntryController.shared.entries[(indexPath as NSIndexPath).row]
+        let entry = EntryController.shared.entries[indexPath.row]
         
         cell.textLabel?.text = entry.title
 
