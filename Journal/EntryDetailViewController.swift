e9050c3dd081e7451142efb6aa482a14f6163cb0
Spencer Curtis <spencercolecurtis@gmail.com>
2017-02-11 20:07:58 -0700

    Update readme with fixes to grammar, as well as add instructions to add an "update(entry: ...)" function that was missing. Without it, the user would not be able to have the changes to their entries persist.
    Update addEntry function to take in the parameters needed to initialize an Entry, instead of having the Entry be initialized in a view controller.
    
    Add an "update(entry: ...)" function in the EntryController to match the instructions.
    
    Update saveButtonTapped IBAction to safely unwrap the text from the text field and view, and now calls the update(entry: ..." function.
    
    The "updateViews" function on the EntryDetailViewController was crashing the app when being called in the didSet of the entry property. It appears the view isn't loaded prior to calling the didSet. When the function was called, it would crash because the textFields were nil. Now, it will only be called in the didSet if the view is already loaded. Otherwise, the viewDidLoad will call it.

--- Journal/EntryDetailViewController.swift
+++ Journal/EntryDetailViewController.swift
@@ -9,61 +9,59 @@
 import UIKit
 
 class EntryDetailViewController: UIViewController, UITextFieldDelegate {
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		updateViews()
-	}
-	
-	// MARK: Actions
-	
-	@IBAction func saveButtonTapped(_ sender: Any) {
-		
-		if let entry = self.entry {
-			entry.title = self.titleTextField.text!
-			entry.text = self.bodyTextView.text
-			entry.timestamp = Date()
-		} else {
-			let newEntry = Entry(title: self.titleTextField.text!, text: self.bodyTextView.text)
-			EntryController.shared.add(entry: newEntry)
-			entry = newEntry
-		}
-		
-		let _ = self.navigationController?.popViewController(animated: true)
-	}
-	
-	@IBAction func clearButtonTapped(_ sender: Any) {
-		
-		titleTextField.text = ""
-		bodyTextView.text = ""
-	}
-	
-	// MARK: Private
-	
-	private func updateViews() {
-		guard let entry = entry else { return }
-		titleTextField.text = entry.title
-		bodyTextView.text = entry.text
-	}
-	
-	// MARK: UITextFieldDelegate
-	
-	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-		
-		textField.resignFirstResponder()
-		
-		return true
-	}
-	
-	// MARK: Properties
-	
-	var entry: Entry? {
-		didSet {
-			updateViews()
-		}
-	}
-	
-	@IBOutlet weak var titleTextField: UITextField!
-	@IBOutlet weak var bodyTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateViews()
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        
+        guard let title = titleTextField.text, let text = bodyTextView.text else { return }
+        
+        if let entry = self.entry {
+            EntryController.shared.update(entry: entry, with: title, text: text)
+        } else {
+            EntryController.shared.addEntryWith(title: title, text: text)
+        }
+        
+        let _ = self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: Any) {
+        
+        titleTextField.text = ""
+        bodyTextView.text = ""
+    }
+    
+    // MARK: Private
+    
+    private func updateViews() {
+        guard let entry = entry else { return }
+        titleTextField.text = entry.title
+        bodyTextView.text = entry.text
+    }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    // MARK: Properties
+    
+    var entry: Entry? {
+        didSet {
+            if isViewLoaded { updateViews() }
+        }
+    }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
 }
